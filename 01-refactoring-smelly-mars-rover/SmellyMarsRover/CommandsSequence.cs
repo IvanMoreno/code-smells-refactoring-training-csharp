7c59c202e7a4194902b67978986c79e2a24f5616
Ivan Moreno Garcia-Fresneda <ivan.m@klang-games.com>
2025-12-15 14:15:22 +0100

    refactor: format commands sequence

--- 01-refactoring-smelly-mars-rover/SmellyMarsRover/CommandsSequence.cs
+++ 01-refactoring-smelly-mars-rover/SmellyMarsRover/CommandsSequence.cs
@@ -5,18 +5,12 @@ using System.Linq;
 namespace SmellyMarsRover;
 
 public record CommandsSequence(IEnumerable<RoverCommand> Commands) : IEnumerable<RoverCommand> {
+    public IEnumerator<RoverCommand> GetEnumerator() => Commands.GetEnumerator();
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    
     public static implicit operator CommandsSequence(string encodedCommands) => new(ParseCommands(encodedCommands));
-
     public static IEnumerable<RoverCommand> ParseCommands(string encodedCommands)
     {
         return encodedCommands.Select(i => RoverCommandMapper.CreateInstance(i.ToString()));
-    }
-
-    public IEnumerator<RoverCommand> GetEnumerator() {
-        return Commands.GetEnumerator();
-    }
-
-    IEnumerator IEnumerable.GetEnumerator() {
-        return GetEnumerator();
     }
 }
