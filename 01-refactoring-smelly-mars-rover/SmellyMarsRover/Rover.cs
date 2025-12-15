c680f445d5e6d91365fbb946ae565bdae763d284
Ivan Moreno Garcia-Fresneda <ivan.m@klang-games.com>
2025-12-15 14:16:23 +0100

    refactor: format rover class

--- 01-refactoring-smelly-mars-rover/SmellyMarsRover/Rover.cs
+++ 01-refactoring-smelly-mars-rover/SmellyMarsRover/Rover.cs
@@ -1,60 +1,41 @@
 ﻿using System;
 using System.Linq;
 
-namespace SmellyMarsRover
-{
-    public class Rover
-    {
+namespace SmellyMarsRover {
+    public class Rover {
         Coordinates coordinates;
         Direction direction;
 
-        public Rover(int x, int y, string direction)
-        {
+        public Rover(int x, int y, string direction) {
             this.direction = DirectionMapper.CreateInstance(direction);
             coordinates = new(x, y);
         }
-        
-        public void Receive(CommandsSequence commandsSequence)
-        {
-            commandsSequence.ToList().ForEach(command => command.ExecuteOn(this));
-        }
 
-        public void MoveBackwards() {
-            coordinates = direction.Move(-1, coordinates);
-        }
+        public void Receive(CommandsSequence commandsSequence) => commandsSequence.ToList().ForEach(command => command.ExecuteOn(this));
+        public void MoveBackwards() => coordinates = direction.Move(-1, coordinates);
+        public void MoveForward() => coordinates = direction.Move(1, coordinates);
+        public void RotateRight() => direction = direction.RotateRight();
+        public void RotateLeft() => direction = direction.RotateLeft();
 
-        public void MoveForward() {
-            coordinates = direction.Move(1, coordinates);
-        }
-
-        public void RotateRight() {
-            direction = direction.RotateRight();
-        }
-
-        public void RotateLeft() {
-            direction = direction.RotateLeft();
-        }
-
-        public override bool Equals(object obj)
-        {
-            if (ReferenceEquals(null, obj)) return false;
-            if (ReferenceEquals(this, obj)) return true;
-            if (obj.GetType() != GetType()) return false;
+        public override bool Equals(object obj) {
+            if (ReferenceEquals(null, obj))
+                return false;
+            if (ReferenceEquals(this, obj))
+                return true;
+            if (obj.GetType() != GetType())
+                return false;
             return Equals((Rover)obj);
         }
 
-        protected bool Equals(Rover other)
-        {
+        protected bool Equals(Rover other) {
             return Equals(coordinates, other.coordinates) && Equals(direction, other.direction);
         }
 
-        public override int GetHashCode()
-        {
+        public override int GetHashCode() {
             return HashCode.Combine(coordinates, direction);
         }
 
-        public override string ToString()
-        {
+        public override string ToString() {
             return $"{nameof(coordinates)}: {coordinates}, {nameof(direction)}: {direction}";
         }
     }
