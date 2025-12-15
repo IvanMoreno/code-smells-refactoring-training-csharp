3b2ab9a7d0e8ae27af67105104f735d3a541d404
Ivan Moreno Garcia-Fresneda <ivan.m@klang-games.com>
2025-12-15 14:14:30 +0100

    refactor: inline factory method

--- 01-refactoring-smelly-mars-rover/SmellyMarsRover/CommandsSequence.cs
+++ 01-refactoring-smelly-mars-rover/SmellyMarsRover/CommandsSequence.cs
@@ -5,9 +5,7 @@ using System.Linq;
 namespace SmellyMarsRover;
 
 public record CommandsSequence(IEnumerable<RoverCommand> Commands) : IEnumerable<RoverCommand> {
-    public static implicit operator CommandsSequence(string encodedCommands) => Create(encodedCommands);
-
-    public static CommandsSequence Create(string encodedCommands) => new(ParseCommands(encodedCommands));
+    public static implicit operator CommandsSequence(string encodedCommands) => new(ParseCommands(encodedCommands));
 
     public static IEnumerable<RoverCommand> ParseCommands(string encodedCommands)
     {
