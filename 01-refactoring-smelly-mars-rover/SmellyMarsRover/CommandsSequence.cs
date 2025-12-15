6a0ceb75215843386df3e5ae6c0dbd85fe475c8e
Ivan Moreno Garcia-Fresneda <ivan.m@klang-games.com>
2025-12-15 14:14:19 +0100

    refactor: extract commands sequence class

---
+++ 01-refactoring-smelly-mars-rover/SmellyMarsRover/CommandsSequence.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SmellyMarsRover;
+
+public record CommandsSequence(IEnumerable<RoverCommand> Commands) : IEnumerable<RoverCommand> {
+    public static implicit operator CommandsSequence(string encodedCommands) => Create(encodedCommands);
+
+    public static CommandsSequence Create(string encodedCommands) => new(ParseCommands(encodedCommands));
+
+    public static IEnumerable<RoverCommand> ParseCommands(string encodedCommands)
+    {
+        return encodedCommands.Select(i => RoverCommandMapper.CreateInstance(i.ToString()));
+    }
+
+    public IEnumerator<RoverCommand> GetEnumerator() {
+        return Commands.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator() {
+        return GetEnumerator();
+    }
+}
