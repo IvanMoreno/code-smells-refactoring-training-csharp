a3a749cb83020788780aa5138b70660c6775932e
Ivan Moreno Garcia-Fresneda <ivan.m@klang-games.com>
2025-12-15 14:48:54 +0100

    refactor: extract base movement class

--- 01-refactoring-smelly-mars-rover/SmellyMarsRover/RoverCommand.cs
+++ 01-refactoring-smelly-mars-rover/SmellyMarsRover/RoverCommand.cs
@@ -11,11 +11,15 @@ public abstract record RoverCommand {
         public override void ExecuteOn(Rover rover) => rover.RotateRight();
     }
     
-    internal record MoveForward : RoverCommand {
-        public override void ExecuteOn(Rover rover) => rover.Move(1);
+    internal record Move(int Displacement) : RoverCommand {
+        public override void ExecuteOn(Rover rover) {
+            rover.Move(Displacement);
+        }
     }
     
-    internal record MoveBackward : RoverCommand {
-        public override void ExecuteOn(Rover rover) => rover.Move(-1);
+    internal record MoveForward() : Move(1) {
+    }
+    
+    internal record MoveBackward() : Move(-1) {
     }
 }
