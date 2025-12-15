909258588da4852500ffb045480dacf0cd55cc0f
Ivan Moreno Garcia-Fresneda <ivan.m@klang-games.com>
2025-12-15 13:51:43 +0100

    refactor: extract rover command file

--- 01-refactoring-smelly-mars-rover/SmellyMarsRover/Rover.cs
+++ 01-refactoring-smelly-mars-rover/SmellyMarsRover/Rover.cs
@@ -4,27 +4,6 @@ using System.Linq;
 
 namespace SmellyMarsRover
 {
-    public record RoverCommand(string value) {
-        public void Execute2(Rover rover) {
-            if (this.value.Equals("l"))
-            {
-                rover.RotateLeft();
-            }
-            else if (this.value.Equals("r")) // Magic literal
-            {
-                rover.RotateRight();
-            }
-            else if (this.value.Equals("f"))
-            {
-                rover.MoveForward();
-            }
-            else
-            {
-                rover.MoveBackwards();
-            }
-        }
-    }
-
     public class Rover
     {
         Coordinates coordinates;
@@ -70,7 +49,7 @@ namespace SmellyMarsRover
         {
             if (ReferenceEquals(null, obj)) return false;
             if (ReferenceEquals(this, obj)) return true;
-            if (obj.GetType() != this.GetType()) return false;
+            if (obj.GetType() != GetType()) return false;
             return Equals((Rover)obj);
         }
 
---
+++ 01-refactoring-smelly-mars-rover/SmellyMarsRover/RoverCommand.cs
@@ -0,0 +1,22 @@
+﻿namespace SmellyMarsRover;
+
+public record RoverCommand(string value) {
+    public void Execute2(Rover rover) {
+        if (value.Equals("l"))
+        {
+            rover.RotateLeft();
+        }
+        else if (value.Equals("r")) // Magic literal
+        {
+            rover.RotateRight();
+        }
+        else if (value.Equals("f"))
+        {
+            rover.MoveForward();
+        }
+        else
+        {
+            rover.MoveBackwards();
+        }
+    }
+}
