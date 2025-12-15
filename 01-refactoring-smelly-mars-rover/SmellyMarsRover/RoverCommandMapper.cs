f63d40b65edb8016ceea450178c1d7aba47abebf
Ivan Moreno Garcia-Fresneda <ivan.m@klang-games.com>
2025-12-15 14:50:12 +0100

    refactor: extract displacement constant

--- 01-refactoring-smelly-mars-rover/SmellyMarsRover/RoverCommandMapper.cs
+++ 01-refactoring-smelly-mars-rover/SmellyMarsRover/RoverCommandMapper.cs
@@ -1,11 +1,13 @@
 ﻿namespace SmellyMarsRover;
 
 internal static class RoverCommandMapper {
+    private const int DISPLACEMENT_LENGTH = 1;
+    
     public static RoverCommand CreateInstance(string encodedCommand) =>
             encodedCommand switch {
                     "l" => new RoverCommand.RotateLeft(),
                     "r" => new RoverCommand.RotateRight(),
-                    "f" => new RoverCommand.Move(1),
-                    _ => new RoverCommand.Move(-1)
+                    "f" => new RoverCommand.Move(DISPLACEMENT_LENGTH),
+                    _ => new RoverCommand.Move(-DISPLACEMENT_LENGTH)
             };
 }
