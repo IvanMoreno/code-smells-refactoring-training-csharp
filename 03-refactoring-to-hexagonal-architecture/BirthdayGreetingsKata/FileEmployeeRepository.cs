594db4eb01e49b58496fbf91cf15f527930f140c
IvanMoreno <ivanmorenogarciafresneda@gmail.com>
2025-12-16 05:41:10 +0100

    refactor: split loop

--- 03-refactoring-to-hexagonal-architecture/BirthdayGreetingsKata/FileEmployeeRepository.cs
+++ 03-refactoring-to-hexagonal-architecture/BirthdayGreetingsKata/FileEmployeeRepository.cs
@@ -16,10 +16,16 @@ public class FileEmployeeRepository
     {
         using var reader = new StreamReader(fileName);
         reader.ReadLine(); // skip header - Smell
-        var employees = new List<Employee>();
+        var lines = new List<string>();
         while (reader.ReadLine() is { } str)
         {
-            var employeeData = str.Split(", ");
+            lines.Add(str);
+        }
+
+        var employees = new List<Employee>();
+        foreach (var line in lines)
+        {
+            var employeeData = line.Split(", ");
             var employee = new Employee(employeeData[1], employeeData[0], employeeData[2], employeeData[3]);
             employees.Add(employee);
         }
