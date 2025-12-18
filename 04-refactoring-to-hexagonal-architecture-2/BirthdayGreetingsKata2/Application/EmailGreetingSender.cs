66bebfd384ba4e7c5169109df1221dadcb219190
IvanMoreno <ivanmorenogarciafresneda@gmail.com>
2025-12-18 06:02:02 +0100

    refactor: move instance method to email greeting sender

--- 04-refactoring-to-hexagonal-architecture-2/BirthdayGreetingsKata2/Application/EmailGreetingSender.cs
+++ 04-refactoring-to-hexagonal-architecture-2/BirthdayGreetingsKata2/Application/EmailGreetingSender.cs
@@ -4,6 +4,28 @@ namespace BirthdayGreetingsKata2.Application;
 
 public class EmailGreetingSender
 {
+    public void SendMessage(string smtpHost, int smtpPort, string sender,
+        string subject, string body, string recipient)
+    {
+        // Create a mail session
+        var smtpClient = new SmtpClient(smtpHost)
+        {
+            Port = smtpPort
+        };
+
+        // Construct the message
+        var msg = new MailMessage
+        {
+            From = new MailAddress(sender),
+            Subject = subject,
+            Body = body
+        };
+        msg.To.Add(recipient);
+
+        // Send the message
+        SendMessage(msg, smtpClient);
+    }
+
     // made protected for testing :-(
     public virtual void SendMessage(MailMessage msg, SmtpClient smtpClient)
     {
